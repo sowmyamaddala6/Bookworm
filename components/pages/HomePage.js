22f51745dc90edc191e5f7a2c88cedd16135edcf
sowmyamaddala6 <sowmyamaddala6@gmail.com>
2020-08-08 15:37:18 +0530

    first commit

---
+++ components/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const HomePage = () => (
+    <div>
+        <h1>Home Page</h1>
+        <link to="/login">Login</link>
+    </div>
+);
+
+export default HomePage;
+
